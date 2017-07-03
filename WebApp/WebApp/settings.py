25ecdd835238fa3312943472f6c656286fc11b82
Carl Castello <carlcastello@yahoo.com>
2017-07-02 20:24:03 -0600

    allowed local host

--- WebApp/WebApp/settings.py
+++ WebApp/WebApp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '0b)%l=o+b3i29h4ccgxq4(na80kl6t_-w-66svt2ekvkzw_jeq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['35.182.168.31']
+ALLOWED_HOSTS = ['35.182.168.31','127.0.0.1']
 
 
 # Application definition

2dd9df2415349345e735afc386221ae8c02cdeba
Carl Castello <carlcastello@yahoo.com>
2017-07-15 19:15:58 -0600

    modified wsgi

--- WebApp/WebApp/wsgi.py
+++ WebApp/WebApp/wsgi.py
@@ -8,8 +8,17 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# add your project directory to the sys.path
+project_home = os.path.join(BASE_DIR, 'WebApp')
+if project_home not in sys.path:
+    sys.path.append(project_home)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "WebApp.settings")
 

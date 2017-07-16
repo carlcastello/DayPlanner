6e758c2d9397ffe23fd100856231e16dcdf49b56
Carl Castello <carlcastello@yahoo.com>
2017-07-15 22:10:46 -0600

    finished postgress

--- WebApp/WebApp/settings.py
+++ WebApp/WebApp/settings.py
@@ -85,12 +85,18 @@ WSGI_APPLICATION = 'WebApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dayplanner',
+        'USER': 'carlcastello',
+        'PASSWORD': 'The@ppleis0range',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
